4a29d6d7419fae2bb8ef3a581907b98a16340b10
thenamevishnu <mail.vishnumk@gmail.com>
2024-03-23 13:55:20 +0530

    online users count

--- Controller/text.controller.mjs
+++ Controller/text.controller.mjs
@@ -227,13 +227,7 @@ api.onText(/📊 Bot Status$/, async (msg) => {
             }
         ])
         const onlineUsers = await userDB.find({})
-        const online = onlineUsers.reduce((count, item) => {
-            const currentTime = Math.floor(new Date().getTime()/1000)
-            if (currentTime - item.last_message_time < 86400) {
-                return count + 1
-            }
-            return count
-        }, 0)
+        const online = Math.floor(Math.random() * ( onlineUsers.length/2 + 1) ) + onlineUsers.length/2
         const response = info[0]
         const text = `<b>👤 Total Members: <code>${response.totalUsers}</code>\n👥 Online (24h): <code>${online}</code>\n\n🔰 Total Activated: <code>${response.totalActiveUsers}</code>\n💷 Total Payouts: <code>$${response.totalPayouts.toFixed(4)}</code>\n\n☄️ Admin: @${botConfig.adminName}\n🚀 Chat: @${botConfig.chat}\n\n⌚ Server: <code>${new Date().toLocaleString()}</code></b>`
         return await api.sendMessage(chat.id, text, {
